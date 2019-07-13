c8ace765d54ed22aa98f9457e1458bdc6fb4280b
efrat <efrat@Efrats-MacBook-Air.local>
2019-07-14 02:00:10 +0300

    change stdout

--- src/Logger.js
+++ src/Logger.js
@@ -27,7 +27,7 @@ export default class Logger {
   }
 
   updateProgress(current) {
-    console.log(this.progress.update(current, this.progressLength));
+    process.stdout(this.progress.update(current, this.progressLength));
   }
 
   getSpinnerStatus(discoveredFiles = 0) {
